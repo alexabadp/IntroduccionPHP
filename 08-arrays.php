11688bee7d462170bee4b5922d54bb1e486d6c13
alexabadp <aalex.abad96@gmail.com>
2023-05-03 09:46:11 -0500

    manejo de arrays

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,5 +1,17 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Computadora'];
+
+// Util para ver los contenidos de un array
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+// Acceder a un elemento del array
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
 
 
 
