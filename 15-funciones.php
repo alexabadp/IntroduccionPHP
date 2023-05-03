290b84a07ebfc26f5e67d223741d84cd04f98e7a
alexabadp <aalex.abad96@gmail.com>
2023-05-03 11:56:00 -0500

    funciones

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,14 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
 
+include 'includes/header.php';
 
+function sumar(int $numero1, int $numero2 = 0)
+{
+    echo $numero1 + $numero2;
+}
 
+sumar(10, 20);
 
 include 'includes/footer.php';
